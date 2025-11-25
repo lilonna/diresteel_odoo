395e3c3d25503d47114804e735caa71c3c466a75
lilonna <lidiyahaile60@gmail.com>
2025-11-25 08:40:19 +0300

    hide

---
+++ custom_addons/hide_apps_menu/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Hide Apps Menu",
+    "version": "1.0",
+    "summary": "Removes the Apps menu for all non-admin users",
+    "author": "lilo",
+    "category": "Tools",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [
+        "views/hide_apps_menu.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
