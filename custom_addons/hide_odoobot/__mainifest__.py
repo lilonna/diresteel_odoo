8a317e58a0493f244d028fc8cd2e9adcd1e497a1
lilonna <lidiyahaile60@gmail.com>
2025-11-25 09:06:19 +0300

    new file:   custom_addons/hide_odoobot/__mainifest__.py 	new file:   custom_addons/hide_odoobot/views/hide_odoobot.xml

---
+++ custom_addons/hide_odoobot/__mainifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Hide OdooBot",
+    "version": "1.0",
+    "summary": "Removes OdooBot for all non-admin users",
+    "category": "Tools",
+    "author": "You",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [
+        "views/hide_odoo_bot.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
